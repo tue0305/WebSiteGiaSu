73a14c42825da96133f130ca988151a094ed175a
nguyencaotan2607 <1751010131tan@ou.edu.vn>
2020-10-18 18:45:36 +0700

    tui fix lai trang dang ky

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ app.use(bodyparser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public/')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/admin', adminRouter);
